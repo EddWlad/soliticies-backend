2e8a7b29c8a5165dc9c6d4a290389cd029fec039
EddWlad <tidsec_inf@outlook.com>
2025-02-26 14:03:30 -0500

    Proyecto Gestion Solicitudes

---
+++ src/main/java/com/tidsec/solicities_service/SolicitiesServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tidsec.solicities_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolicitiesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolicitiesServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tidsec/solicities_service/SolicitiesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tidsec.solicities_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolicitiesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
