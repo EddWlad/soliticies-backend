96a9e9f4ef337b1587bce531b2675894a1d459c4
EddWlad <tidsec_inf@outlook.com>
2025-04-23 22:11:58 -0500

    CRUD Company

--- src/main/java/com/tidsec/solicities_service/controller/CompanyController.java
+++ src/main/java/com/tidsec/solicities_service/controller/CompanyController.java
@@ -1,20 +1,29 @@
 package com.tidsec.solicities_service.controller;
 
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
 import com.tidsec.solicities_service.dtos.CompanyDTO;
 import com.tidsec.solicities_service.entities.Company;
 import com.tidsec.solicities_service.entities.MediaFileLogo;
 import com.tidsec.solicities_service.services.ICompanyService;
 import com.tidsec.solicities_service.util.MapperUtil;
 import lombok.RequiredArgsConstructor;
+import org.cloudinary.json.JSONObject;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.net.URI;
 import java.util.Base64;
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
@@ -24,6 +33,7 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 @RequiredArgsConstructor
 public class CompanyController {
 
+    private final Cloudinary cloudinary;
     private final ICompanyService companyService;
     private final MapperUtil mapperUtil;
 
@@ -67,6 +77,20 @@ public class CompanyController {
         return ResponseEntity.noContent().build();
     }
 
+    @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<List<String>> upload(@RequestParam("file") MultipartFile multipartFile) throws Exception {
+
+        File f = convertToFile(multipartFile);
+        Map<String, Object> response =  cloudinary.uploader().upload(f, ObjectUtils.asMap("resource_type", "auto"));
+        JSONObject json = new JSONObject(response);
+        String url = json.getString("url");
+
+        System.out.println(url);
+
+        return ResponseEntity.ok(Collections.singletonList(url));
+
+    }
+
     // 📌 6️⃣ Obtener empresa con HATEOAS
     @GetMapping("/hateoas/{id}")
     public EntityModel<CompanyDTO> findByIdHateoas(@PathVariable("id") Long id) throws Exception  {
@@ -79,5 +103,13 @@ public class CompanyController {
         resource.add(link1.withRel("company-self-info"));
         resource.add(link2.withRel("company-all-info"));
         return resource;
+    }
+
+    private File convertToFile(MultipartFile multipartFile) throws Exception{
+        File file = new File(multipartFile.getOriginalFilename());
+        FileOutputStream outputStream = new FileOutputStream(file);
+        outputStream.write(multipartFile.getBytes());
+        outputStream.close();
+        return file;
     }
 }
